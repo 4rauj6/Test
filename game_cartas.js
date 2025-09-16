3215044a813ed2fb831b75dbc0c136ef0ecbcacf
João Victor <159325010+4rauj6@users.noreply.github.com>
2025-09-16 16:20:17 -0300

    Add files via upload

--- game_cartas.js
+++ game_cartas.js
@@ -1,4 +1,15 @@
+function home_page() {
+    document.getElementById("voltar_home").style.display = "none";
+}
+
 function mostrar_perfil() {
     document.getElementById("perfil_player").style.display = "block";
+    document.getElementById("select").style.display = "none";
+    document.getElementById("voltar_home").style.display = "none";
+}
+
+function iniciar_jogo() {
     document.getElementById("top_navbar").style.display = "none";
+    document.getElementById("select").style.display = "none";
+    document.getElementById("voltar_home").style.display = "block";
 }
